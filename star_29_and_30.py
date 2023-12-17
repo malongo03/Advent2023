4966ea3c14e790c6d0afe6129f087148702f1c5b
malongo03 <mlongo2021@gmail.com>
2023-12-16 22:53:45 -0500

    Added Days 14 and 15

---
+++ star_29_and_30.py
@@ -0,0 +1,66 @@
+def hash_algorithm(line):
+    """
+    Calculates the HASH value of an input string.
+
+    Input:
+        line [str]
+
+    Return int
+    """
+    current_value = 0
+    for char in line:
+        current_value = ((current_value + ord(char)) * 17) % 256
+    return current_value
+
+def initialize_lenses(filename):
+    """
+    Takes in a list of instructions for initializing the lenses of the lava
+    production facility (see Day 15 of the Advent of Code 2023). It prints the
+    sum of the HASH values of the initialization instructions, and then
+    simulates the initialization sequence to find the intensity of the outputted
+    light.
+
+    Input:
+        filename [str]: The filename of the input
+    """
+    with open(filename, encoding = "utf-8") as f:
+        sequence = f.read().split(",")
+
+    star_1_answer = 0
+    for code in sequence:
+        star_1_answer += hash_algorithm(code)
+    print(f"The answer to Star 29 is {star_1_answer}")
+
+
+    box_dict = {box: [] for box in range(256)}
+    for code in sequence:
+        index = 0
+        label = ""
+        while True:
+            if code[index] == "=" or code[index] == "-":
+                operation = code[index]
+                break
+            else:
+                label += code[index]
+            index += 1
+        box = hash_algorithm(label)
+        existing_labels = [p_label for p_label, _ in box_dict[box]]
+        if label in existing_labels:
+            replace_index = existing_labels.index(label)
+            if operation == "=":
+                new_lens = int(code[index + 1])
+                box_dict[box][replace_index] = [label, new_lens]
+            else:
+                box_dict[box].pop(replace_index)
+        elif operation == "=":
+            new_lens = int(code[index + 1])
+            box_dict[box] = box_dict[box] + [[label, new_lens]]
+
+    star_2_answer = 0
+    for box, lens_list in box_dict.items():
+        for index, lens_w_label in enumerate(lens_list):
+            _, lens_power = lens_w_label
+            star_2_answer += (box + 1) * (index + 1) * lens_power
+    print(f"The answer to Star 30 is {star_2_answer}")
+
+initialize_lenses("day_15.txt")
